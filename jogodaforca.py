da1407510002ac27f59b99f3679a2c5d15f90bba
isabellycotes-design <isabelly.cotes@escola.pr.gov.br>
2026-08-06 15:10:50 +0000

    oiii

--- jogodaforca.py
+++ jogodaforca.py
@@ -1,19 +1,38 @@
 #Jogo da forca
-print("***
+print("*********************************)
 print ("Bem vindo ao jogo da Foca")
-print(************************
-**********)
-palavrasecreta = "forte"
-letrasacertadas - ["=", "_", "_",
-enforcou - False acertou - False
-while(not enforcou and not acertou) :
-chute = input( "Digite uma letra? ")
-chute = chute. strip()
-*-*1
-index = 0
-for letra in palavrasecreta:
-1f(chute.upper() == letra.upper()) :
-print( "Encontrei a letra () na posição ()"-format(letra, index))
-index - index + 1
-print( "jogando")
-print( "Fim do Jogo")
+print(**********************************)
+
+palavrasecreta = "Abacaxi".upper
+letrasacertadas - ["_"] * len(palavrasecreta)
+
+print(letrasacertadas)
+
+ enforcou = False
+ acertou = False
+
+while(not enforcou and not acertou and tentativas < 5) :
+    chute = input( "Digite uma letra? ") 
+    chute = chute.strip()
+
+if(chute in palavrasecreta):
+    index = 0
+    for letra in palavrasecreta:
+     if(chute.upper() == letra.upper()) :
+        print( "Encontrei a letra {} na posição {}".format(letra, index))
+    index = index + 1
+else:
+     tentativas += 1
+     
+     # controle de tentativas 
+     enforcou = tentativas == total_tentativas 
+     acertou = "_" not in letrasacertadas                                                                                                                                                                    tentativas += 1
+     print("letras acertadas: {}".format(letrasacertadas))
+     print("tentativas restantes: {}".format(total_tentativas - tentativas))
+
+     if(arcertou):
+        print("parabéns, você ganhou!")
+     elif(enforcou):
+        print("você perdeu! A palavra era {}".format(palavrasecreta))  
+
+ print( "Fim do Jogo")
