f71b82fc642593216a6dfff674f11ba222eb7555
isabellycotes-design <isabelly.cotes@escola.pr.gov.br>
2026-08-06 14:17:12 +0000

    oii

---
+++ jogodaforca.py
@@ -0,0 +1,19 @@
+#Jogo da forca
+print("***
+print ("Bem vindo ao jogo da Foca")
+print(************************
+**********)
+palavrasecreta = "forte"
+letrasacertadas - ["=", "_", "_",
+enforcou - False acertou - False
+while(not enforcou and not acertou) :
+chute = input( "Digite uma letra? ")
+chute = chute. strip()
+*-*1
+index = 0
+for letra in palavrasecreta:
+1f(chute.upper() == letra.upper()) :
+print( "Encontrei a letra () na posição ()"-format(letra, index))
+index - index + 1
+print( "jogando")
+print( "Fim do Jogo")
